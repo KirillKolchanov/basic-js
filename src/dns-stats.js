7970ef4d19314365650713e6923d0e52b882a68f
KirillKolchanov <kirill.kolchanov.work@gmail.com>
2022-04-30 00:15:26 +0200

    feat: solve dns stats

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,29 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ function getDNSStats(domains) {
+
+  let result = {};
+
+  for (let i = 0; i < domains.length; i++) {
+
+    let arr = domains[i].split(".").reverse();
+
+    let domain = "";
+
+    for (let i = 0; i < arr.length; i++) {
+
+      domain += "." + arr[i];
+
+      if (result.hasOwnProperty(domain)) {
+        result[domain]++;
+      } else {
+        result[domain] = 1;
+      }
+      //console.log(domain)
+    }
+  }
+  return result;
 }
 
 module.exports = {
