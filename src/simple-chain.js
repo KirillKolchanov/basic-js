43d30ac56f753ef1a18a5b5fc42c258fd346bf6e
KirillKolchanov <kirill.kolchanov.work@gmail.com>
2022-04-30 18:52:56 +0200

    feat: solve simple chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -2,28 +2,35 @@ const { NotImplementedError } = require('../extensions/index.js');
 
 /**
  * Implement chainMaker object according to task description
- * 
+ *
  */
 const chainMaker = {
+  arr: [],
+
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.arr.length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value) {
+    this.arr.push("( " + value + " )");
+    return this;
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  removeLink(position) {
+    const i = position - 1;
+    if (this.arr[i] === undefined) {
+      this.arr = [];
+      throw new Error(`You can't remove incorrect link!`);
+    }
+    this.arr.splice(i, 1);
+    return this;
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.arr.reverse();
+    return this;
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    const finishedChain = this.arr.join("~~");
+    this.arr = [];
+    return finishedChain;
   }
 };
 
