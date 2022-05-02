a46c59998085d50a2ff916fde5b7fe6d4580af51
KirillKolchanov <kirill.kolchanov.work@gmail.com>
2022-05-02 23:50:09 +0200

    feat: solve vigenere cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -19,14 +19,78 @@ const { NotImplementedError } = require('../extensions/index.js');
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
  *
  */
+
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  constructor(value) {
+    if (value === false) {
+      this.direct = value;
+    } else {
+      this.direct = true;
+    }
+    this.alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  setKeyLength(message, key) {
+    let count = 0;
+    return message.toUpperCase().split('').map((letter) => {
+      if (this.alphabet.includes(letter)) {
+        return key[count++ % key.length].toUpperCase();
+      } else {
+        return letter;
+      }
+    })
+  }
+
+  encrypt(message, key) {
+    if (!message || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+
+    const newKey = this.setKeyLength(message, key);
+    const result = [];
+
+    message.toUpperCase().split('').forEach((letter, i) => {
+      if (this.alphabet.includes(letter)) {
+        let j = (this.alphabet.indexOf(letter) + this.alphabet.indexOf(newKey[i])) % 26;
+        result.push(this.alphabet[j]);
+      } else {
+        result.push(letter);
+      }
+    })
+
+      if (this.direct) {
+        return result.join('');
+      } else {
+        return result.reverse().join('');
+      }
+  }
+
+  decrypt(message, key) {
+    if (!message || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+
+    const newKey = this.setKeyLength(message, key);
+    const result = [];
+
+    message.toUpperCase().split('').forEach((letter, i) => {
+      if (this.alphabet.includes(letter)) {
+        let j = this.alphabet.indexOf(letter) - (this.alphabet.indexOf(newKey[i]) % 26);
+        if (j < 0) {
+          j = 26 + j;
+        }
+        result.push(this.alphabet[j]);
+      } else {
+        result.push(letter);
+      }
+    })
+
+    if (this.direct) {
+      return result.join('');
+    } else {
+      return result.reverse().join('');
+    }
   }
 }
 
